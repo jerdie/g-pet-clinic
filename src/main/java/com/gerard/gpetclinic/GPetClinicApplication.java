9f57cbdd4630249f5facc86046a370a58f4383d1
jerdie <5143814+jerdie@users.noreply.github.com>
2020-07-31 00:02:08 +0800

    init project

---
+++ src/main/java/com/gerard/gpetclinic/GPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.gerard.gpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gerard/gpetclinic/GPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerard.gpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
