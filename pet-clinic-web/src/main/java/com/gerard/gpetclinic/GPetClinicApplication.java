c2bd701f86b69498ff098da369ed704b27668e83
jerdie <5143814+jerdie@users.noreply.github.com>
2021-08-12 00:13:14 +0800

    add wro4j processing

--- pet-clinic-web/src/main/java/com/gerard/gpetclinic/GPetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/gerard/gpetclinic/GPetClinicApplication.java
@@ -1,22 +1,13 @@
 package com.gerard.gpetclinic;
 
-import com.gerard.gpetclinic.config.TestConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class GPetClinicApplication {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(GPetClinicApplication.class, args);
-
-        TestConfig testConfig = ctx.getBean(TestConfig.class);
-        System.out.println("GPetClinicApplication ------");
-        System.out.println("TestConfig value1");
-        System.out.println(testConfig.getValue1());
-        System.out.println("TestConfig value2");
-        System.out.println(testConfig.getValue2());
+        SpringApplication.run(GPetClinicApplication.class, args);
     }
 
 }
--- pet-clinic-web/src/main/java/com/gerard/gpetclinic/config/PetClinicConfig.java
+++
@@ -1,9 +0,0 @@
-package com.gerard.gpetclinic.config;
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-
-@EnableConfigurationProperties(TestConfig.class)
-@Configuration
-public class PetClinicConfig {
-}
