2d55eaa78aa1546b1031bdb1749f82d0f5f3e97e
jerdie <5143814+jerdie@users.noreply.github.com>
2021-08-25 22:40:53 +0800

    Add specialties, added to Vet.

---
+++ pet-clinic-web/src/main/java/com/gerard/gpetclinic/person/vet/Specialty.java
@@ -0,0 +1,16 @@
+package com.gerard.gpetclinic.person.vet;
+
+import com.gerard.gpetclinic.common.BaseEntity;
+
+public class Specialty extends BaseEntity {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
--- pet-clinic-web/src/main/java/com/gerard/gpetclinic/person/vet/Vet.java
+++ pet-clinic-web/src/main/java/com/gerard/gpetclinic/person/vet/Vet.java
@@ -2,5 +2,17 @@ package com.gerard.gpetclinic.person.vet;
 
 import com.gerard.gpetclinic.person.Person;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Specialty> specialties;
+
+    public Set<Specialty> getSpecialties() {
+        return specialties;
+    }
+
+    public void setSpecialties(Set<Specialty> specialties) {
+        this.specialties = specialties;
+    }
 }
