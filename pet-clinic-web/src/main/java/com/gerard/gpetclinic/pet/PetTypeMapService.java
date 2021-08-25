910e4bae325f692eaf296b62a83edbb70b26646e
jerdie <5143814+jerdie@users.noreply.github.com>
2021-08-25 22:50:04 +0800

    added PetTypeService

---
+++ pet-clinic-web/src/main/java/com/gerard/gpetclinic/pet/PetTypeMapService.java
@@ -0,0 +1,34 @@
+package com.gerard.gpetclinic.pet;
+
+import com.gerard.gpetclinic.common.AbstractMapService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType,Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long aLong) {
+        return super.findById(aLong);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteById(aLong);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+}
---
+++ pet-clinic-web/src/main/java/com/gerard/gpetclinic/pet/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.gerard.gpetclinic.pet;
+
+import com.gerard.gpetclinic.common.CrudService;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
